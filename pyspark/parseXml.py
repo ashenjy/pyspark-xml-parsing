be5588f48316bfafc9f3af4e73a9d736cee0f110
ashen <jayasinghe.ashen@gmail.com>
2021-08-07 17:41:11 +0530

    adding xml parsing logic

---
+++ pyspark/parseXml.py
@@ -0,0 +1,13 @@
+import sys
+
+from pyspark.sql import SparkSession
+
+xmlFIlePath = "/home/ashen/Desktop/GSB-Project/Client-Documents/NEW-GSB-FILES/Inputs/asdas/RC11_20210120234500/PM202101202330+110024LNBTS.xml"
+
+spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+
+df = spark.read.format("com.databricks.spark.xml").option("rowTag", "measInfo").load(xmlFIlePath).cache()
+
+dfRows = df.count()
+
+print("df row count: " + str(dfRows))
