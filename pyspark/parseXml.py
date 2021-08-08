cea44c4ccc94bf8beede5c9d5db223a98f0b0381
ashen <jayasinghe.ashen@gmail.com>
2021-08-08 17:26:58 +0530

    adding xml parsing logic changes

--- pyspark/parseXml.py
+++ pyspark/parseXml.py
@@ -4,7 +4,10 @@ from pyspark.sql import SparkSession
 
 xmlFIlePath = "/home/ashen/Desktop/GSB-Project/Client-Documents/NEW-GSB-FILES/Inputs/asdas/RC11_20210120234500/PM202101202330+110024LNBTS.xml"
 
-spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+spark = SparkSession.builder\
+    .appName("XMLParseApp")\
+    .master("spark://ashen-ubuntu-os:7077")\
+    .getOrCreate()
 
 df = spark.read.format("com.databricks.spark.xml").option("rowTag", "measInfo").load(xmlFIlePath).cache()
 
